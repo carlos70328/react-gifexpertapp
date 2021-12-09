068cfad570e2c63dba1a3171ca41da178d5b856f
Carlos Florez <carlosflo70328@gmail.com>
2021-12-09 15:23:45 -0500

    docs agregado

---
+++ docs/precache-manifest.0571d01bb4ce02370978d61e56940757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25eebf75460536332fe1a8ce6684569a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0a1cf2f5567803f2958",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a1cf2f5567803f2958",
+    "url": "/static/js/main.f9440e07.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
